687025c8ffc280df6c4acb6a18c6413bc3c9e685
JoshHayden <jhayden@ithaca.edu>
2020-12-15 15:29:41 -0500

    added innate sort

--- src/main/java/com/williamfiset/algorithms/sorting/KotlinInnateSort.kt
+++ src/main/java/com/williamfiset/algorithms/sorting/KotlinInnateSort.kt
@@ -1,4 +1,23 @@
 package com.williamfiset.algorithms.sorting
 
-class KotlinInnateSort {
+import kotlin.jvm.JvmStatic
+
+class KotlinInnateSort : InplaceSort {
+    override fun sort(values: IntArray) {
+        innateSort(values)
+    }
+
+    companion object {
+        private fun innateSort(ar: IntArray) {
+            ar.sort()
+        }
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val sorter: InplaceSort = InsertionSort()
+            val array = intArrayOf(10, 4, 6, 8, 13, 2, 3)
+            sorter.sort(array)
+            println(array.contentToString())
+        }
+    }
 }
